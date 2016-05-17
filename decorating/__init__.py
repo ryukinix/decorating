32ddf8c7c2df78a94e740b7dddb196ce52dda351
Manoel Vilela <manoel_vilela@engineer.com>
2016-05-17 19:16:15 -0300

    Wow, an wrong URL here! mal -> decorating project
    HAHA, the evil of copy & paste.

--- decorating/__init__.py
+++ decorating/__init__.py
@@ -12,7 +12,7 @@ from decorating.animation import animated
 __version__ = '0.1-alpha'
 __author__ = 'Manoel Vilela'
 __email__ = 'manoel_vilela@engineer.com'
-__url__ = 'https://github.com/ryukinix/mal'
+__url__ = 'https://github.com/ryukinix/decorating'
 
 
 __all__ = ['animated']
