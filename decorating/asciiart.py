03bb4eb1ef31a727c6618433f12c11eefa49d41d
Manoel Vilela <manoel_vilela@engineer.com>
2016-05-24 03:43:18 -0300

    Add new asciiart baed on gitsome
    Why? Because ASCIIART é wonderful, as well, is nothing more
    about the god Lain.

--- decorating/asciiart.py
+++ decorating/asciiart.py
@@ -5,13 +5,50 @@
 #
 #   Copyright © Manoel Vilela
 #
+#                  ..        .;-:!!>>!!:--;..  .
+#               .     .-:!>7?CO$QQQQ$OC?:--::-.....
+#             .   .-!7?CO$QQQQQ$$$OCCCC?!:--:>>:;;;...
+#           .  .->CCCC>?OO$$OO$$$$$$O$$$O?7>:>C$C7!:-;;.  .
+#         .  .!?O$Q$$OOCC?CCO$QQQQQQ$$QQQ$$$OO$QNHH$C?>!:;  .
+#        . .!OQQQHHHQQQQQ$$QQQQHQHQQQQQHQQHHHHHHHNNNHHQ$OC>.   .
+#       . ;CQHHHHHHHHHHHHHHHHHHHHHHHQHHHHHHNNNNNNNNHNHNNHHH$!;
+#        :OHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHNNNNNNNNNHHHHNNHNNO:-.
+#       !OHNNNNNHHHHHHHHHHHHHHHHHHHHHHHHNNHHNNNNNNHHHHHHNHNNNH>;;.
+#    . !OHNHHHHNHHHHHHHHHHHHHHHNNNNNNNHHHNNNNNNNNHNMNNNNHNNNHNN7  .
+#     -OQNHNNNNNNHNHHNNHHNNNNNNNNNNNNNNHHHNNNNNHHNQ>!>OHNNHNNHHN$;
+#    .?$HHNNNNNNNNNNHHNHHNNNNNNNNNNNNNNNNNNNNNNHNQ.;>:::CNNHNNHHNQ!.
+#  . :$HNNNNNNHHNNHHNNNNHNNNNNNNNNNHHNHNNNNNNHHHN? !;  . -ONHNNHNO!-....
+#   .?QHNNNNNNNNNNNNNNNNNNNNNNNNNHHNNHNHHNNNNNNNNC .;->!:..QNHHHN$!:-;.
+# . :$HNNNHNNNNNNNNNNNNNNNNNNNNHHNM$QNHNNHNNNHQQNO  :$O>!- CNHHHHN$O7...
+# . !QHNNNNNNNNNNNNNNNNNNNHHNNHNNO>;;?:7QNNNQQHQQH! .O7!; :HHHHHNNNH:...
+# . !HHNNNNNNNNNNNNNNNNNNNHHHHH$>;.-!7C?O$HOHNNNNO?; -... CMHNNNHQH> ...
+# . !HHNNNNNNNNNNNNNNNNNNNHHNN$>77OHNNHHQ7OC$QHHQ>;..  ..;$QOCOQOO!.....
+# . !NNNNNNNNNNNNNNNNHHHHNHNNHC$CHO?Q$QQ? ?HQHQ$O$- .. .:??!-. :>- .....
+#   .QNHNNNNNNNNNNNNNNNNNHHHN$!:-?; :7!.  ;>7NNNHQ7 .   .;;;..;:.......
+#    ONHNNNNNNNNNNNNHO$HHMNNM?     .        .!HNHQO.      ;-.-:;....
+#  . 7NHHNHNNNNNNNNHN?:!;>7:!!.      ..    .. 7NHQQ: .  ;;::>:;;.....
+#    -!HNHHNNNNNNNNNNQ?>:;.   .             . .QNQQ? . ;>7C?:::.........
+#      7NHNNHNNNNNHNQCCO$Q$O:.               . ?MHQ$.  >QO>!>7;.........
+#    . .QHNNNNNHHHHHH$QHQNQ$>...             . >NHHN7 ;QO!>CC-..........
+#       :-$HO$ONHHNHNNHO->CC; .;.            . 7NHHH$;?$>7O$:.......  ..
+#         -$:--QHNQN>CMNQ:;--;;;....      .... ?NHNQ!OHCOOQ>;;;.....   .
+#        . 7:  ;OH>?;.77-  ;;..:-      .....   ONHNQCHHHQHQ!--......  ..
+#          .. . --.;;.       . ;O>:; .;........>$NNHNNNHHHH?-.... .   ..
+#                ;..  ......;.;->>:::--;; .;..!>CNNQQNHNHNQ!....
+#             . ;:. .......... .;:->?   -: .-.!HHHN7>NHHNH>;...      .
+#              .!; ...   ........;!ONC>>C$7?O>7QNHNQ?HNNNO:...         .
+#             :C!.;.   .... ...;-!?$NNNNNNNNNNNHHNHNNHHNQ7;...      ....
+#            7$O-.. .        ...!$QNNHHHHHHHHHHHHNNHHNNQC-...     ......
+#          .?Q$:......       ...-$NNNNHHHHNHNNNHHNNNNNQC>;..     .......
+
+
 """
     This is another LOL-zone
 
     LOOOOOOOOOOOOOL ART
 """
 
-
+# START
 #     /\O    |    _O    |      O
 #      /\/   |   //|_   |     /_
 #     /\     |    |     |     |\
@@ -34,3 +71,12 @@ SQUARES = "◰◳◲◱"
 TRIANGULES = "◢◣◤◥"
 VPULSE = '▁▂▃▄▅▆▇▆▅▄▃▁'
 WAVE = "⠁⠂⠄⡀⢀⠠⠐⠈"
+
+
+# END
+#     /\O    |    _O    |      O
+#      /\/   |   //|_   |     /_
+#     /\     |    |     |     |\
+#    /  \    |   /|     |    / |
+#  LOL  LOL  |   LLOL   |  LOLLOL
+# -----------+----------+-----------
